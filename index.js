1cfedf3ed37c83f805f1d80daa1c33d18e65c11e
elizabethrsotomayor <elizabethrsotomayor@gmail.com>
2023-01-11 16:59:35 -0600

    add JSXDemo

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-class Hello extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello there!</h1>
-        <p>This is my first React class component</p>
-      </div>
-    );
-  }
-}
+// class Hello extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello there!</h1>
+//         <p>This is my first React class component</p>
+//       </div>
+//     );
+//   }
+// }
 
 // function Hello() {
 //   return (
@@ -18,4 +18,19 @@ class Hello extends React.Component {
 //   );
 // }
 
-ReactDOM.render(<Hello />, document.getElementById("root"));
+function getMood() {
+  const moods = ["Angry", "Hungry", "Silly", "Quiet", "Lonely"];
+  return moods[Math.floor(Math.random() * moods.length)];
+}
+
+class JSXDemo extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>My current mood is: {getMood()}</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<JSXDemo />, document.getElementById("root"));
